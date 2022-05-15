7e28c0f6dd8c96df23dc82562ea0540cf983969d
ac79b0c6 <105047743+ac79b0c6@users.noreply.github.com>
2022-05-15 20:20:01 +0800

    update

--- mkvtool/docker/run.sh
+++ mkvtool/docker/run.sh
@@ -1,5 +1,5 @@
 NAME="mkvtool"
-[ "$(docker images | grep ${NAME})" ] && docker build -t ${NAME} $(dirname "$0")
+[ "$(docker images | grep ${NAME})" ] || docker build -t ${NAME} $(dirname "$0")
 FONT_DIR="/usr/share/fonts/truetype" # Change this to your font directory
 CACHE_DIR="${HOME}/.mkvtool/caches"  # Change this to your cache directory
 OTHER_DIR=""                         # Change this to your other directory for example: -v aaa:bbb
